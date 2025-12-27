79c03e0577a4aa65ef1d215f98d50abb85f6084b
rohini <rohini@DESKTOP-OI9JR7I>
2025-12-27 15:39:11 +0530

    Assignment-1

---
+++ CoreJava_assignments/src/com/rohini/assignment/AssignmentMain.java
@@ -0,0 +1,25 @@
+package com.rohini.assignment;
+
+
+import com.rohini.assignment.employees.Manager;
+import com.rohini.assignment.employees.Developer;
+import com.rohini.assignment.utilities.EmployeeUtilities;
+
+public class AssignmentMain {
+
+    public static void main(String[] args) {
+
+        Manager manager = new Manager();
+        manager.setName("Rohini");
+        manager.setEmployeeId(101);
+        manager.setProjectCount(5);
+
+        Developer developer = new Developer();
+        developer.setName("Anu");
+        developer.setEmployeeId(102);
+        developer.setLanguageKnown("Java");
+
+        EmployeeUtilities.displayEmployee(manager);
+        EmployeeUtilities.displayEmployee(developer);
+    }
+}
