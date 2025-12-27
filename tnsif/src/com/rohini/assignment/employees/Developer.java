88e962139d30e466fa067c1d707363ec28326389
rohini <rohini@DESKTOP-OI9JR7I>
2025-12-27 14:29:56 +0530

    Assignment-1

---
+++ tnsif/src/com/rohini/assignment/employees/Developer.java
@@ -0,0 +1,14 @@
+package com.rohini.assignment.employees;
+
+public class Developer extends Employee {
+	private String languageKnown;
+
+	public String getLanguageKnown() {
+		return languageKnown;
+	}
+
+	public void setLanguageKnown(String languageKnown) {
+		this.languageKnown = languageKnown;
+	}
+	
+}
---
+++ tnsif/src/com/rohini/assignment/employees/Employee.java
@@ -0,0 +1,27 @@
+package com.rohini.assignment.employees;
+
+public class Employee {
+	private String name;
+	private int employeeId;
+	private double salary;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
